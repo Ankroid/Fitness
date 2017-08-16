57e95a02cfc47e4ac810a81885bd5b32f4bfee15
Ankroid <ankurbadola01@gmail.com>
2017-08-16 16:16:59 +0530

    TEst

---
+++ app/src/main/java/com/example/kh2063/mygitpushproject/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.kh2063.mygitpushproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(getApplicationContext(),"Test",Toast.LENGTH_LONG).show();
+    }
+}
